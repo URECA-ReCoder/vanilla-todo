ab5c57f6f210da4ea41c38a17c1bd3f390cf293a
jissssu <113964353+jissssu@users.noreply.github.com>
2024-08-27 11:16:08 +0900

    기본 기능 추가

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-// 다들 화이팅!! ٩( *˙0˙*)۶
-//test commit
+  const todoInput = document.querySelector(".input-container input");
+  const addButton = document.querySelector(".input-container button");
+  const todoList = document.querySelector(".todo-list");
+  const todoCount = document.querySelector(".todo-list-container h2");
+
+  let todoItems = [];
